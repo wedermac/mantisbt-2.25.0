40a7d60fdcccfae65ba212776a518fbe90ad30c6
Weder M <wedermac@hotmail.com>
2021-03-23 00:53:33 -0300

    first commit

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = '10.0.0.191:5432';
+$g_db_type                = 'pgsql';
+$g_database_name          = 'mantis';
+$g_db_username            = 'admin';
+$g_db_password            = 'systec2011.';
+
+$g_default_timezone       = 'America/Sao_Paulo';
+
+$g_crypto_master_salt     = 'm3yOKeCmdn5pUmJXVNEtrJZz03qHXTy7B6xWzHR0TYo=';
